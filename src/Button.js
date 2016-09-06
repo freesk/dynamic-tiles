d60b0c92a4ff0b229d23007ed451fc47e10022cf
Dmitry <dmitr.kurbatov@gmail.com>
2016-09-06 13:22:01 +0300

    comments

--- src/Button.js
+++ src/Button.js
@@ -5,9 +5,11 @@ class Button {
     this._htmlElem.innerHTML = text;
     this._value = value;
   }
+  // Add class active to the button htmlElem
   setAsActive() {
     this._htmlElem.classList.add('active');
   }
+  // Remove class active to the button htmlElem
   setAsInactive() {
     this._htmlElem.classList.remove('active');
   }
